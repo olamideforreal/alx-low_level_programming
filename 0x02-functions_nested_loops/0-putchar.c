ba746e7b1312e8252b29d2cc004e36233c525b84
olamideforreal <colamide2@gmail.com>
2022-03-16 13:34:17 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
