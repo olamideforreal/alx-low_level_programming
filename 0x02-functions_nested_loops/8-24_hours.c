53b806208089da549d4a6d572ee2a02472375b85
olamideforreal <colamide2@gmail.com>
2022-03-16 13:44:14 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			_putchar(i / 10 + 48);
-			_puchar(i % 10 +48);
+			_putchar(i % 10 +48);
 			_putchar(':');
 			_putchar(j / 10 + 48);
 			_putchar(j % 10 + 48);
