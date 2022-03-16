fa00dd03a4466c27f402453bff49f2bb4a0f8f9b
olamideforreal <colamide2@gmail.com>
2022-03-16 13:25:13 -0700

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int print_alst_digit(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		r = -1 * (n % 10);
+		_putchar(r + '0');
+		return (r);
+	}
+	else
+	{
+		r = n % 10;
+		_putchar(r + '0');
+		return (r);
+	}
+}
