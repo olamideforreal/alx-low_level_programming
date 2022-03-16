56b610235d2b169d9122deaebd9c31a14a2a292a
olamideforreal <colamide2@gmail.com>
2022-03-16 13:40:45 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24, i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + 48);
+			_puchar(i % 10 +48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
+		}
+	}
+}
