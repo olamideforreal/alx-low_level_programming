3aa943791e677bc510d8153ca46979e7cd5d9587
olamideforreal <colamide2@gmail.com>
2022-03-18 03:16:51 -0700

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print line
+ * draws a straight line in the terminal
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
